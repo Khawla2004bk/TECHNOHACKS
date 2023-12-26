592112cf80cbc8c2d4393f73476f39f56cb0e25c
khawla <boukniter.khawla@gmail.com>
2023-12-26 19:02:35 +0100

    Create a Python program that allows users to convert temperatures between Fahrenheit and Celsius. The program includes error handling for non-numeric temperature input and invalid unit input. Update README.md with usage instructions, error handling details, an example, and other relevant information.

---
+++ Temperature_convrter/functions.py
@@ -0,0 +1,33 @@
+
+def main():    
+    while True:
+        try:
+            T = float(input("Enter the temperature to convert: "))
+            break
+        except ValueError:
+            print("Invalid input. Please enter a valid temperature value: ")
+        
+    while True:
+        try:
+            unit = input("Choose the unit to convert to; \"F\" for Fahrenheit and \"C\" for Celsius: ").upper()
+
+            if unit == "F":
+                T1 = (T - 32) * 5/9
+                print(f"{T} F is equal to {T1:.2f} C")
+                break
+
+            elif unit == "C":
+                T1 = (T * 9/5) + 32
+                print(f"{T} C is equal to {T1:.2f} F")
+                break
+
+            else:
+               print("Invalid unit. Please enter 'F' or 'C'.")
+    
+        
+    
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
